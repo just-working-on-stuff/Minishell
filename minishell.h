a1f3a34083afbbf2a2721f5945f9565a6301f85f
Ghaalya Saad <ghsaad@lab2r4s2.42abudhabi.ae>
2025-09-30 18:28:30 +0400

    g

--- minishell.h
+++ minishell.h
@@ -18,6 +18,26 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+/* ===================== TOKENS ===================== */
+
+# define TOK_CMD       0
+# define TOK_ARG       1
+# define TOK_PIPE      2
+# define TOK_REDIR_IN  3
+# define TOK_REDIR_OUT 4
+# define TOK_APPEND    5
+# define TOK_HEREDOC   6
+
+/* ===================== ERROR MACROS ===================== */
+# define ERR_MALLOC 1
+# define ERR_MALLOC "Memory allocation failed\n"
+
+# define ERR_PIPE 	1
+# define ERR_PIPE 	"Pipe creation failed\n"
+
+# define ERR_FORK 	1	
+# define ERR_FORK 	"Fork failed\n"	
+
 typedef struct s_token
 {
     char *str;
@@ -57,44 +77,38 @@ typedef struct s_data
 }	t_data;
 
 
-/* ===================== TOKENS ===================== */
-
-# define TOK_CMD       0
-# define TOK_ARG       1
-# define TOK_PIPE      2
-# define TOK_REDIR_IN  3
-# define TOK_REDIR_OUT 4
-# define TOK_APPEND    5
-# define TOK_HEREDOC   6
 
 /*================signals===============*/
 
 void	setup_parent_signals(void);
 void	setup_child_signals(void);
-int run_simple_command(char *const argv[], char *const envp[], t_shell_state *state);
+int		run_simple_command(char *const argv[], char *const envp[], t_shell_state *state);
 
-void disable_echoctl(void);
+void	disable_echoctl(void);
 
 /* ===================== PARSING ===================== */
 
-t_token *lexer(char *line);
-t_cmd   *parser(t_token *tokens, char **envp);
-char    *expand_value(char *word, char **envp, int last_exit);
-
-void    free_token(t_token **list);
-void    free_cmds(t_cmd *cmds);
+t_token		*lexer(char *line);
+t_cmd		*parser(t_token *tokens, char **envp);
+char		*expand_value(char *word, char **envp, int last_exit);
+void		free_token(t_token **list);
+void		free_cmds(t_cmd *cmds);
 
 // int     handle_heredoc(char *delimiter);
-t_token *new_token(char *str, int type);
-void add_token_back(t_token **lst, t_token *new);
+t_token		*new_token(char *str, int type);
+void		add_token_back(t_token **lst, t_token *new);
 // void    print_tokens(t_token *lst);
-void parse_redir(t_cmd *cmd, t_token *tok);
-t_cmd *parser(t_token *tokens, char **envp);
-void	pars_word(t_cmd *cmd, t_token *tok);
-void add_cmd_back(t_cmd **lst, t_cmd *new);
-t_token *lexer(char *line);
-t_cmd *new_cmd(void);
+void 		parse_redir(t_cmd *cmd, t_token *tok);
+t_cmd		*parser(t_token *tokens, char **envp);
+void		pars_word(t_cmd *cmd, t_token *tok);
+void 		add_cmd_back(t_cmd **lst, t_cmd *new);
+t_token		*lexer(char *line);
+t_cmd		*new_cmd(void);
 
-
+//list_token.c
+int			append_token(t_token **list, char *str, int type);
+void    	free_token(t_token **list);
+static void add_first(t_token **list, t_token *new);
+static int  token_new_element(t_token **new, char *str, int type);
 
 #endif
