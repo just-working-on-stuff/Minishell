b5a55d38f4ad077643783077c6efa27621524f85
Random_queso <159764203+just-working-on-stuff@users.noreply.github.com>
2025-09-30 12:58:38 +0000

    echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -12,3 +12,31 @@
 
 #include "minishell.h"
 
+int	ft_echo(char **args)
+{
+	int	i;			
+	int	newline_flag;
+	
+	i = 1;			// Start from 1 to skip the command name "echo"
+	newline_flag = 1; // By default, we want to print a newline at the end
+	if (!args[1]) // If no arguments are provided, just print a newline
+	{
+		printf("\n");
+		return (0);
+	}
+	while (args[i] && args[i][0] == '-' && args[i][1] == 'n' && args[i][2] == '\0')
+	{
+		newline_flag = 0; // If we find -n, we set the flag to not print a newline
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1]) // If there's another argument, print a space
+			printf(" ");
+		i++;
+	}
+	if (newline_flag) // If the -n flag was not found, print a newline
+		printf("\n");
+	return (0);
+}
