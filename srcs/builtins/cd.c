65f13b4599588ca03bd26a3809ce02383d0ec783
Ghaalya Saad <ghsaad@lab2r4s2.42abudhabi.ae>
2025-10-15 17:55:11 +0400

    changed to cd and created export builtin

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ghsaad <ghsaad@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 17:07:42 by ghsaad            #+#    #+#             */
-/*   Updated: 2025/10/13 16:25:35 by ghsaad           ###   ########.fr       */
+/*   Updated: 2025/10/15 16:56:48 by ghsaad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int ft_cd(char **args)
         if (chdir(home) == -1)
         {
             perror("cd");
-            return (1);
+            return (1); //more clarification, so cd why? 
         }
     }
     // Handle cd with path argument
@@ -48,6 +48,12 @@ int ft_cd(char **args)
         perror("");
         return (1);
     }
+     /*
+this else if conditional checks if the chdir function fails when trying to change the directory to the path specified in args[1].
+If chdir returns -1, it indicates an error occurred (like the directory doesn't exist or permission is denied).
+In that case, it prints an error message to standard error (file descriptor 2) that includes "cd: ", the problematic path, and the specific error message from perror.
+for example, writing cd: /nonexistent: No such file or directory     
+    */
 
     // Get new working directory after change
     if (!getcwd(new_pwd, PATH_MAX))
@@ -55,7 +61,14 @@ int ft_cd(char **args)
         perror("cd: getcwd");
         return (1);
     }
-
+/*
+to test this in the actual bash,
+for the if (!getcwd(new_pwd, PATH_MAX)) part,
+type cd /nonexistent_directory in bash and it should
+print an error message like "cd: /nonexistent_directory: No such file or directory"
+why perror("cd: getcwd")? because getcwd failed to get the current working directory
+its not printed because getcwd is not called if chdir fails
+*/
     // Update PWD and OLDPWD environment variables
     // Note: You'll need to implement update_env() function
     update_env("OLDPWD", old_pwd);
@@ -70,7 +83,4 @@ and keeps the pwd and oldpwd environment variables updated so that it knows wher
 the system
 */
 
-update_env(char *key, char *value)
-{
-    
-}
+
